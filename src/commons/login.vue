20afbbf6e9adb3fab72d34c2dcd167127194d6af
wst-21 <365628316@qq.com>
2020-03-11 20:51:26 +0800

    1

--- src/commons/login.vue
+++ src/commons/login.vue
@@ -16,24 +16,55 @@
              <span>|</span>
              <span>账户登录</span>
            </div>
+           <!-- 输入框 -->
            <div class="login_input">
              <div class="account">
                <span>
                  <i class="el-icon-user"></i>
                </span>
-               <input v-model="account" type="text">
+               <input v-model="account" @blur="zhang" type="text">
                <span>
-                 <i class="el-icon-circle-close"></i>
+                 <i v-show="one" class="el-icon-circle-close"></i>
                </span>
+             </div>
+             <div class="mark">
+               <span v-if="one">请输入账号</span>
              </div>
              <div class="pass">
                <span>
                  <i class="el-icon-lock"></i>
                </span>
-               <input v-model="account" type="text">
+               <input v-model="pass" @blur="mi" type="text">
                <span>
                  <i class="el-icon-circle-close"></i>
                </span>
+             </div>
+             <div class="mark">
+               <span v-if="two">请输入密码</span>
+             </div>
+           </div>
+           <!-- 验证 -->
+           <div class="yan">
+             <span>短信验证登录</span>
+             <span>忘记密码</span>
+           </div>
+           <!-- 按钮 -->
+           <div class="btn">
+             <button>登录</button>
+           </div>
+           <!-- 微信 -->
+           <div class="xin">
+             <span></span>
+           </div>
+           <div class="list">
+             <div class="list_ul">
+               <span>新浪微博</span>
+               <span>|</span>
+               <span>QQ</span>
+               <span>|</span>
+               <span>支付宝</span>
+               <span @click="btnChange">{{change}}</span>
+               <span>免费注册</span>
              </div>
            </div>
         </div>
@@ -50,12 +81,39 @@ export default {
   data(){
     return{
       account:null,
-      pass:null
+      pass:null,
+      one:false,
+      two:false,
+      change:"更多"
     }
   },
   methods:{
     home(){
       this.$router.push('/')
+    },
+    zhang(){
+      if(this.account){
+        this.one = false
+      }
+      else{
+        this.one = true
+      }
+    },
+    mi(){
+      if(this.pass){
+        this.two = false
+      }
+      else{
+        this.two = true
+      }
+    },
+    btnChange(){
+      if(this.change = "更多"){
+        this.change = "收起"
+      }
+      else{
+        this.change = '更多'
+      }
     }
   }
 };
@@ -107,6 +165,9 @@ export default {
   align-items: center; */
   /* border: 1px solid black; */
 }
+.login_input{
+  margin-top: 40px;
+}
 .login-round{
   /* border: 1px solid black; */
   margin-right: auto;
@@ -148,7 +209,7 @@ export default {
   width: 85%;
   height: 48px;
   border: 1px solid gray;
-  margin: 35px auto;
+  margin: 5px auto;
   border-radius: 2px;
   /* line-height: 48px;/ */
   font-size: 25px;
@@ -166,5 +227,60 @@ i{
 }
 .el-icon-user,.el-icon-lock{
   color: gray;
+}
+.mark{
+  margin-left: 30px;
+  font-size: 14px;
+  color: rgb(245, 37, 37);
+  height: 20px;
+}
+.yan,.btn{
+  width: 85%;
+  margin: 5px auto;
+  display: flex;
+  justify-content: space-between;
+  /* border: 1px solid black; */
+  height: 40px;
+  font-size: 15px;
+}
+.btn>button{
+  width: 100%;
+  height: 100%;
+  color: #ffffff;
+  background: rgb(218, 54, 54);
+  outline: none;
+  font-size: 18px;
+  cursor: pointer;
+  outline: none;
+}
+.yan span:hover{
+  cursor: pointer;
+  color: red;
+}
+.yan span:nth-child(2){
+  font-size: 12px;
+}
+.xin{
+  width: 85%;
+  border-bottom: 1px solid gray;
+  margin: 40px auto;
+  
+}
+.xin span{
+  background-image: url("../assets/sprites-hash-c9975078.png");
+  width: 40px;
+  height: 40px;
+}
+.list{
+  width: 85%;
+  margin: 0 auto;
+}
+.list_ul{
+  display: flex;
+  justify-content: space-between;
+  font-size: 14px;
+}
+.list_ul span{
+  cursor: pointer;
 }
 </style>

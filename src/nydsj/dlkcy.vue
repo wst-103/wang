3f2e3d0d3b4bd4fea487938f89766e1891f3ea16
wst-21 <365628316@qq.com>
2020-03-17 23:00:50 +0800

    模板

--- src/nydsj/dlkcy.vue
+++ src/nydsj/dlkcy.vue
@@ -1,15 +1,43 @@
 <template>
-  <div class="dlkjj">
-      电力看产业
+  <div class="dlkjj">电力看产业
+    <p v-for="(i,index) in list" :key="index">{{i}}</p>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      arr: ["王法", "贺莹", "张雪莉", "郭萍萍"],
+      list:[],
+      timer:null,
+      a:0
+    };
+  },
+  methods:{
+    change(){
+      this.list.push(this.arr[0]);
+       var len = this.arr.length;
+        console.log(len);
+        console.log(this.list)
+      this.timer = setInterval(()=>{
+        this.list = []
+        this.list.push(this.arr[this.a]);
+        if(this.a < 3){
+           this.a = this.a+1
+          //  console.log(this.a);
+        }else{
+          this.a = 0
+          console.log(this.a);
+        }
+      },3000)
+    },
+  },
+  created(){
+    this.change()
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/nydsj/dlkms.vue
+++ src/nydsj/dlkms.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="dlkjj">
       电力看民生
+      <button @click="btn">点击</button>
+      <input type="file" hidden ref="input" @change="btnChange">
+      <img ref="img" src="" alt="">
   </div>
 </template>
 
 <script>
 export default {
-
+  computed:{
+     file(){
+       return this.$refs.input
+     }
+  },
+  methods:{
+    btn(){
+      this.file.click()
+      
+      
+    },
+    btnChange(){
+      const a = this.file.files[0]
+      console.log(a);
+      // 使用window.URL.createObjectURL()得到file数据
+      const fileData = window.URL.createObjectURL(a)
+      this.$refs.img.src = fileData
+    }
+  }
 }
 </script>
 
